18f7e6bf449950b7d982c0e7264a668285e2f231
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-16 18:54:41 -0800

    input functions for host and port

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
-host = "127.0.0.1" #need to change for connecting to a server not on your computer
-port = 4096 #port: need to connect to the server port
+host = input("Enter the server host IP (127.0.0.1 if it is your computer): ")#"127.0.0.1" #need to change for connecting to a server not on your computer
+port = input("Enter the server port number (4096 for this demonstration): ")#4096 #port: need to connect to the server port
 
 socket = socket.socket()
 
