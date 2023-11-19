aa5ecc9960f10d36a66d4df6cf68c20e2f2aa0d1
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-19 13:17:59 -0800

    comments with explanations

--- multiconnect server.py
+++ multiconnect server.py
@@ -13,14 +13,14 @@ socket.setblocking(False) #eliminates the blocking problem
 
 selector.register(socket, selectors.EVENT_READ, data = None)
 
-def accept_client(socket):
-  connect, address = socket.accept()  # Should be ready to read
+def accept_client(socket): #accepts a client and adds them to the selector
+  connect, address = socket.accept()
   connect.setblocking(False)
   data = types.SimpleNamespace(address = address, inbytes = b"", outbytes = b"")
   events = selectors.EVENT_READ | selectors.EVENT_WRITE #| = "or" in bytes
   selector.register(connect, events, data = data)
 
-def check_connection(socket_object, ready_events):
+def check_connection(socket_object, ready_events): #checks connection (if not connected - deletes connection)
   socket = socket_object.fileobj
   data = socket_object.data
   if ready_events and selectors.EVENT_READ: #if in read mode
@@ -38,7 +38,7 @@ def check_connection(socket_object, ready_events):
       print("Sending "+str(sent_data)+" to "+str(socket))
       data.outbytes = data.outbytes[sent_data:] #adds sent_data to out_bytes
 
-try:
+try: #checks if there is any error
   while True:
     events = selector.select(timeout = None)
     for socket_object, ready_events in events:
