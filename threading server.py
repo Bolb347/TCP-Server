520fdc70539973bbe4bda454ea258922d9616048
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-22 08:12:19 -0800

    update to deletion of client (through no data)

--- threading server.py
+++ threading server.py
@@ -12,7 +12,7 @@ socket.listen()
 def thread(client): #threads a client
   while True:
     data = client.recv(1024) #recieves 1024 bytes of data from the given client
-    if data is None: #removes the client if it has disconnected
+    if data == None: #removes the client if it has disconnected
       print("removing thread to client "+client)
       lock.release() #removes the client from threading
       break
