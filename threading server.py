69478ddd6cfd5b29defb3ae3444656db51361269
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-27 14:13:38 -0800

    threading server fix

--- threading server.py
+++ threading server.py
@@ -1,32 +1,37 @@
 import socket, threading
 
-lock = threading.lock()
+lock = threading.Lock()
 
 host = "127.0.0.1"
-port = 2048
+port = 4096
 
 socket = socket.socket()
 socket.bind((host, port))
 socket.listen()
 
 def thread(client): #threads a client
+  print("initializing threading")
   while True:
-    data = client.recv(1024) #recieves 1024 bytes of data from the given client
-    if data == None: #removes the client if it has disconnected
-      print("removing thread to client "+client)
-      lock.release() #removes the client from threading
-      break
+    try:
+      data = client.recv(1024) #recieves 1024 bytes of data from the given client
+      if data == None: #removes the client if it has disconnected
+        print("removing thread to client "+str(client))
+        lock.release() #removes the client from threading
+        break
 
-    data = b"hello" #creates data (needs to be tested for on client side)
-    client.send(data) #sends the data
-    print("sent data: "+data)
+      data = b"hello" #creates data (needs to be tested for on client side)
+      client.send(data) #sends the data
+      print("sent data: "+str(data))
+    except:
+      print("ERR")
   client.close()
+  lock.release()
 
 while True:
   client, address = socket.accept() #accepts the client
-  print("accepted new client"+client+address)
+  print("accepted new client"+str(client)+str(address))
   lock.acquire()
 
-  start_new_thread(thread, (client,)) #creates the thread
+  threading._start_new_thread(thread, (client,)) #creates the thread
 
 socket.close()
