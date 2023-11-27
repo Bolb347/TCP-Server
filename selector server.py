315da99099cbb172094c9071928780534152ac0c
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-27 13:49:35 -0800

    bug with blocking

--- selector server.py
+++ selector server.py
@@ -2,32 +2,32 @@ import socket, selectors, types
 
 selector = selectors.DefaultSelector()
 
-host = 127.0.0.1 #set to the host of the server's ip
+host = "127.0.0.1" #set to the host of the server's ip
 port = 4096 #set to the port to listen to (needs to be larger than 1024)
 
 socket = socket.socket()
 socket.bind((host, port))
 socket.listen() #listens to the socket
 
-socket.setblocking(False) #eliminates the blocking problem
+socket.setblocking(True) #eliminates the blocking problem
 
 selector.register(socket, selectors.EVENT_READ, data = None)
 
-def accept_client(socket): #accepts a client and adds them to the selector
-  connect, address = socket.accept()
+def accept_client(socket):
+  connect, address = socket.accept()  # Should be ready to read
   connect.setblocking(False)
   data = types.SimpleNamespace(address = address, inbytes = b"", outbytes = b"")
   events = selectors.EVENT_READ | selectors.EVENT_WRITE #| = "or" in bytes
   selector.register(connect, events, data = data)
 
-def check_connection(socket_object, ready_events): #checks connection (if not connected - deletes connection)
+def check_connection(socket_object, ready_events):
   socket = socket_object.fileobj
   data = socket_object.data
   if ready_events and selectors.EVENT_READ: #if in read mode
     recieved_data = socket.recv(4096) #recieves a max amount of 4096
     if recieved_data:
       print("Recieved: "+str(recieved_data))
-      data. += recieved_data #adds recieved data to out data
+      data.outbytes += recieved_data #adds recieved data to out data
     else:
       print("Closing connection to"+str(socket))
       selector.unregister(socket)
@@ -38,7 +38,7 @@ def check_connection(socket_object, ready_events): #checks connection (if not co
       print("Sending "+str(sent_data)+" to "+str(socket))
       data.outbytes = data.outbytes[sent_data:] #adds sent_data to out_bytes
 
-try: #checks if there is any error
+try:
   while True:
     events = selector.select(timeout = None)
     for socket_object, ready_events in events:
