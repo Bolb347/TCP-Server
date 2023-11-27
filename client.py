3c29d626b59fa0fa02437c15688ed163f9b3bcb1
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-27 14:15:33 -0800

    client hotfix

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
 import socket
 
-host = input("Enter the server host IP (127.0.0.1 if it is your computer): ")#"127.0.0.1" #need to change for connecting to a server not on your computer
-port = input("Enter the server port number (4096 for this demonstration): ")#4096 #port: need to connect to the server port
+host = "127.0.0.1"#input("Enter the server host IP (127.0.0.1 if it is your computer): ")#"127.0.0.1" #need to change for connecting to a server not on your computer
+port = 4096#input("Enter the server port number (4096 for this demonstration): ")#4096 #port: need to connect to the server port
 
 socket = socket.socket()
 
-socket.connect(host, port)
+socket.connect((host, port))
 
 def send_data(data): #sends data to server (needs to be in byte form before by using b"data")
   socket.sendall(data)
 
-def recieve_bytes: #recieves and returns bytes from the server
+def recieve_bytes(): #recieves and returns bytes from the server
   return socket.recv(4096)
 
 def decode(bytes):
@@ -18,3 +18,5 @@ def decode(bytes):
 
 while True:
   send_data(b"connected")
+  print("recieved data: "+str(decode(recieve_bytes())))
+  print("sent data")
