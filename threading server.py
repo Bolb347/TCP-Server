a1ebaba17eec6c74b6eb9ee756bd094df392d7ee
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-27 21:49:15 -0800

    need to add a list of threads and an identifier for each thread (hotfix)

--- threading server.py
+++ threading server.py
@@ -1,7 +1,5 @@
 import socket, threading
 
-lock = threading.Lock()
-
 host = "127.0.0.1"
 port = 4096
 
@@ -16,7 +14,6 @@ def thread(client): #threads a client
       data = client.recv(1024) #recieves 1024 bytes of data from the given client
       if data == None: #removes the client if it has disconnected
         print("removing thread to client "+str(client))
-        lock.release() #removes the client from threading
         break
 
       data = b"hello" #creates data (needs to be tested for on client side)
@@ -25,12 +22,10 @@ def thread(client): #threads a client
     except:
       break
   client.close()
-  lock.release()
 
 while True:
   client, address = socket.accept() #accepts the client
   print("accepted new client"+str(client)+str(address))
-  lock.acquire()
 
   threading._start_new_thread(thread, (client,)) #creates the thread
 
