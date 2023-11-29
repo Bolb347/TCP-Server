0978299e5e4c7a1a9b384c7dabc9445a7ec93774
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-29 14:25:57 -0800

    threading fix

--- threading server.py
+++ threading server.py
@@ -13,36 +13,41 @@ data_in = []
 threads = []
 
 def thread(client): #threads a client
-  print("initializing threading")
   threads.append(client.fileno())
+  print(threads)
   while True:
     try:
       data = client.recv(1024)
-      data_in.append(types.SimpleNameSpace(data = client.recv(1024), client_fd = client.fileno())) #recieves 1024 bytes of data from the given client
+      #data_in.append(types.SimpleNameSpace(data = client.recv(1024), client_fd = client.fileno())) #recieves 1024 bytes of data from the given client
       if data == None: #removes the client if it has disconnected
-        print("removing thread to client "+str(client))
+        print("!!!")
         break
-
       for data in data_out:
-        if data.client_fd == client.fileno(): #tests for specified client
-          client.send(bytes(data.data)) #sends data
+        if data[0] == client.fileno(): #tests for specified client
+          client.send(bytes(data[1], 'utf-8')) #sends data
+          print("sending data")
     except:
       break
+  print("removing thread to client "+str(client))
   try:
     threads.remove(client.fileno())
   finally:
     pass
   client.close()
 
-while True:
+while True:  
   client, address = socket.accept() #accepts the client
   print("accepted new client"+str(client)+str(address))
 
+  print("checking for threads")
   threading._start_new_thread(thread, (client, )) #creates the thread with (function, arguments)
+  print("adding threads")
 
-  data_out.append(types.SimpleNamespace(data = "abcd", client_fd = input("client file descriptor:")))
 
+  if len(threads):
+    data_out.append((threads[0], "hi"))
+  
+  #data_out = []
   data_in = []
-  data_out = []
 
 socket.close()
