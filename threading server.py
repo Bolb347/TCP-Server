969205505c26986e9dd506f9831b53a04ca0dbfd
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-28 21:37:27 -0800

    specified client sending

--- threading server.py
+++ threading server.py
@@ -21,13 +21,15 @@ def thread(client): #threads a client
   threads.append(client.fileno())
   while True:
     try:
+      data = client.recv(1024)
       data_in.append(types.SimpleNameSpace(data = client.recv(1024), client_fd = client.fileno())) #recieves 1024 bytes of data from the given client
       if data == None: #removes the client if it has disconnected
         print("removing thread to client "+str(client))
         break
 
-      data = b"hello" #creates data (needs to be tested for on client side)
-      client.send(data) #sends the data
+      for data in data_out:
+        if data.client_fd == client.fileno(): #tests for specified client
+          client.send(data.data) #sends data
     except:
       break
   try:
@@ -38,7 +40,9 @@ while True:
   client, address = socket.accept() #accepts the client
   print("accepted new client"+str(client)+str(address))
 
-  threading._start_new_thread(thread, (client,)) #creates the thread
+  threading._start_new_thread(thread, client) #creates the thread with (function, arguments)
+
+  data_out.append(types.SimpleNamespace(data = input("data you want to send:"), client_fd = input("client file descriptor:")))
 
   print("Recieved data: "+data_in)
   data_in = []
