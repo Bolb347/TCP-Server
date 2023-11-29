48cb4654e43f6783c6f3a16b2cf8ea0957421243
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2023-11-29 13:54:15 -0800

    client update

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 host = "127.0.0.1"#input("Enter the server host IP (127.0.0.1 if it is your computer): ")#"127.0.0.1" #need to change for connecting to a server not on your computer
-port = 4096#input("Enter the server port number (4096 for this demonstration): ")#4096 #port: need to connect to the server port
+port = 52000#input("Enter the server port number (4096 for this demonstration): ")#4096 #port: need to connect to the server port
 
 socket = socket.socket()
 
