b8a8730ad185ba7541d3fc3b8fde368d6ce48a45
Bolb347 <147656916+Bolb347@users.noreply.github.com>
2024-03-12 14:55:32 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, time
 from threading import Thread
 
 host = "127.0.0.1"
@@ -16,6 +16,7 @@ threads = []
 def main():
     global data_in, data_out, threads
     while True:
+        time.sleep(1)
         data_in = []
         for thread in threads:
             for i in range(5):
@@ -23,24 +24,29 @@ def main():
 
 def thread(client): #threads a client
     global data_in, data_out, threads
+    broken = False
     threads.append(client.fileno())
     print(threads)
-    data = None
-    try:
-        data = client.recv(1024)
-    except:
-        print("encountered err while recv")
+    while not broken:
+        data = None
+        try:
+            data = client.recv(1024)
+        except:
+            print("encountered err while recv")
+            broken = True
+        
+        data_in.append((client.fileno(), data))
+        for data in data_out:
+            if data[0] == client.fileno(): #tests for specified client
+                try:
+                    client.send(data[1]) #sends data
+                except:
+                    print("encountered err while sending")
+                    broken = True
+                data_out.remove(data)
+                break
     
-    data_in.append((client.fileno(), data))
-    for data in data_out:
-        if data[0] == client.fileno(): #tests for specified client
-            try:
-                client.send(data[1]) #sends data
-            except:
-                print("encountered err while sending")
-            break
-        data_out.remove(data)
-        print("removing thread to client "+str(client))
+    print("removing thread to client "+str(client))
     try:
         threads.remove(client.fileno())
     except:
